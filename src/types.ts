55a565f1b05d70da2ce9962883bf8688439a6dd8
jcvidela3 <juan_videla.acid@skyairline.com>
2024-05-19 23:13:31 -0300

    lint

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,70 @@
 export interface IMetaStockData {
-    symbol: string;
-    interval: string;
-    currency: string;
-    exchange_timezone: string;
-    mic_code: string;
-    exchange: string;
-    type: string;
-  }
-  
-  export interface IValuesStockData {
-    datetime: string;
-    open: string;
-    high: string;
-    low: string;
-    close: string;
-    volume: string;
-  }
-  
-  export interface IStockData {
-    meta: IMetaStockData;
-    values: IValuesStockData[];
-    status: string;
-  }
+  symbol: string;
+  interval: string;
+  currency: string;
+  exchange_timezone: string;
+  mic_code: string;
+  exchange: string;
+  type: string;
+}
 
-  export interface IStock {
-    symbol: string;
-    name: string;
-    currency: string;
-    type: string;
-  }
+export interface IValuesStockData {
+  datetime: string;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+}
+
+export interface IStockData {
+  meta: IMetaStockData;
+  values: IValuesStockData[];
+  status: string;
+}
+
+export interface IStock {
+  symbol: string;
+  name: string;
+  currency: string;
+  type: string;
+}
+interface IOption {
+  value: string;
+  label: string;
+}
+
+export interface IRadioButtonProps {
+  name: string;
+  value: string;
+  checked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+}
+
+export interface IDateInputProps {
+  disabled: boolean;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export interface ISelectInputProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: IOption[];
+}
+
+export interface IButtonProps {
+  type: "button" | "submit" | "reset";
+  children: React.ReactNode;
+}
+
+export interface IIntervalSelectProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+export interface IStockPreferenceFormProps {
+  symbol: string;
+  handleSetStockData: React.Dispatch<React.SetStateAction<IStockData | null>>;
+}
